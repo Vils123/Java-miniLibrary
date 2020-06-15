831922ea1cb95b7f8bbe0f7674f7db33ee7c2217
Wruper <ricards1033@gmail.com>
2020-06-15 17:15:59 +0300

    sdsdsd
    Pec vlada veleta comments pileiku

--- src/main/java/lv/venta/demo/models/bruh.java
+++ src/main/java/lv/venta/demo/models/bruh.java
@@ -1,19 +1,8 @@
 package lv.venta.demo.models;
 
-<<<<<<< HEAD
-public class bruh { 
-
-
-    //22222222222222222222222222222222222222222222222 - testeju Git HUb skolotaj
-=======
 public class bruh {
-<<<<<<< HEAD
     ///33333333333333
 
->>>>>>> bc14d26c51f17d83aed685210e4a55ea4aceb62a
-=======
-    ///111111111111 bruh bruh bruh bruh bruh
-
->>>>>>> 50624599c18b02cab8c58a68a13f67441e50a7c5
 
 }
+/// rIČARDS IR DORSH CHALITIS ;)
